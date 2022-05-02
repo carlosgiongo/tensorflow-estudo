b5c0403eb8bd91f039d1d29a821c1d0ca86b4645
Carlos Giongo <carlosgiongoo@gmail.com>
2022-05-02 16:20:10 -0300

    Ajustado input

--- index.js
+++ index.js
@@ -2,20 +2,30 @@ const tf = require('@tensorflow/tfjs-node');
 
 async function main (){
     const model = tf.sequential()
-    model.add(tf.layers.dense({units:1, inputShape: [1]}))
+    model.add(tf.layers.dense({units:1, inputShape: [4]}))
     
     model.compile({
         loss: 'meanSquaredError',
         optimizer: 'sgd'
     })
     
-    const xs = tf.tensor2d([-1, 0, 1, 2, 3, 4], [6,1])
-    const ys = tf.tensor2d([-3, -1, 1, 3, 5, 7], [6,1])
+    const xs = tf.tensor2d([ 
+        [1, 0, 0, 1], 
+        [1, 1, 1, 0],
+        [1, 0, 0, 0],
+        [0, 0, 0, 0],
+        [1, 1, 1, 1],
+        [1, 0, 1, 0],
+        [1, 1, 0, 0],
+        [0, 1, 1, 1] 
+    ])  //8 coleções de 4 elementos
+    const ys = tf.tensor2d([[2], [3], [1], [0], [4], [2], [2], [3]]) //8 coleções de 1 elementos
     
-    await model.fit(xs, ys, {epochs: 500})
+    await model.fit(xs, ys, {epochs: 750})
     
-    let result = model.predict(tf.tensor2d([20], [1,1]))
-    console.log(result.dataSync())
+    let result = model.predict(tf.tensor2d([1, 1, 0, 1], [1, 4]))
+    let dado = await result.dataSync()
+    console.log("Resultado: " + Math.round(dado))
 }
 
 main()
